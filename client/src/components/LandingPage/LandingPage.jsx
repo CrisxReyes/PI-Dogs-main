70334116adfd372e57618d6a875571c1ad8c663f
CrisxReyes <cristopher.smnyl@gmail.com>
2022-08-16 16:12:19 -0500

    landing page lista, faltan iconos de git y linkedin

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import  s from'./LandingPage.module.css';
-
-const image = require('../../img/landing_dogs.jpg').default;
+import icon from '../../img/git_icon.png';
 
 export default function LandingPage(){
     return (
         <div className={s.container}>
-            <button>HOME</button>
+             <div className={s.navLandingPage}>
+                <a href="https://github.com/CrisxReyes">
+                     <img className={s.github} src={icon} alt="" />
+                </a>
+            </div>
+            <div className={s.info}>
+                <h1>DOGSPEDIA</h1>
+                <p>Encuentra toda la información acerca de tu raza favorita.</p>
+                <NavLink id={s.boton_home} class="btn btn-primary btn-lg" to="/home">Comenzar</NavLink>
+            </div>
+            
         </div>
     )
 }
